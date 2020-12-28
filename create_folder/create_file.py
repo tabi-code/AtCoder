0361f59ca779c52eb5dbcd72d75f233554209201
tabi-code <pgsideworks@gmail.com>
2020-12-28 18:44:05 +0900

    202012281845

--- create_folder/create_file.py
+++ create_folder/create_file.py
@@ -1,1 +1,11 @@
-print("hello")
+import os
+
+for i in range(126):
+    for j in range(65, 70):
+        folder = "C:/Dropbox/vscode/AtCoder/AtCoder/ABC/ABC" + ("000" + str(i))[-3:]
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        path = folder + "/ABC" + ("000" + str(i))[-3:] + chr(j) + ".py"
+        f = open(path, 'w')
+        f.write('')  # 何も書き込まなくてファイルは作成されました
+        f.close()
